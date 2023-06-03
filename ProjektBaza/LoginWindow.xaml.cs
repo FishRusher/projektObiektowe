fedf315cbab4db0375db5e6ccf48fb9055c1e315
FishRusher <szprot19@gmail.com>
2023-06-03 12:35:14 +0200

    database connection

--- ProjektBaza/LoginWindow.xaml.cs
+++ ProjektBaza/LoginWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +30,22 @@ namespace ProjektBaza
         {
             string login = loginInput.Text;
             string pass = passInput.Password;
+            pass = MD5Generator.FromString(pass);
+
+            var connection = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=projekt_baza");
+            connection.Open();
+            var command = new MySqlCommand("Select * from users where login = '" + login + "' and pass = '" + pass + "';",connection);
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                IncorrectLoginMessage.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                IncorrectLoginMessage.Visibility = Visibility.Visible;
+            }
+
         }
     }
 }
---
+++ ProjektBaza/MD5Generator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektBaza
+{
+    class MD5Generator
+    {
+        public static string FromString(string s)
+        {
+            var encoder = MD5.Create();
+            byte[] tmp = Encoding.ASCII.GetBytes(s);
+            byte[] bytes = encoder.ComputeHash(tmp);
+
+            StringBuilder result = new StringBuilder(bytes.Length * 2);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result.Append(bytes[i].ToString("x2"));
+            }
+
+            return result.ToString();
+        }
+    }
+}
